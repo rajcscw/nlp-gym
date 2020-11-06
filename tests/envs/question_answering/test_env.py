61815ec28196dc3cc90fd07b3cca820d4f453df9
Rajkumar R <raj1514@gmail.com>
2020-11-06 20:50:05 +0100

    Updated tests and some  cleanup

--- tests/envs/question_answering/test_env.py
+++ tests/envs/question_answering/test_env.py
@@ -13,19 +13,49 @@ def sample():
 
 @pytest.fixture
 def env():
-    env = QAEnv()
+    env = QAEnv(return_obs_as_vector=False)
     return env
 
 
 def test_correct_answer(env, sample):
-    observation = env.reset()
+    observation = env.reset(sample)
     assert observation.get_facts() == sample.facts
     assert observation.get_question() == sample.question
-    assert observation.get_choice() == "A"
+    assert observation.get_choice() == "Answer text A"
+    observation, reward, done, info = env.step(env.action_space.action_to_ix("CONTINUE"))
+    assert observation.get_facts() == sample.facts
+    assert observation.get_question() == sample.question
+    assert observation.get_choice() == "Answer text B"
+    assert reward == 0.0
+    observation, reward, done, info = env.step(env.action_space.action_to_ix("ANSWER"))
+    assert reward == 1.0
+    assert done
 
 
 def test_incorrect_answer(env, sample):
-    pass
+    observation = env.reset(sample)
+    assert observation.get_facts() == sample.facts
+    assert observation.get_question() == sample.question
+    assert observation.get_choice() == "Answer text A"
+    observation, reward, done, info = env.step(env.action_space.action_to_ix("ANSWER"))
+    assert reward == 0.0
+    assert done
+
 
 def test_out_of_choices(env, sample):
-    pass
+    observation = env.reset(sample)
+    assert observation.get_facts() == sample.facts
+    assert observation.get_question() == sample.question
+    assert observation.get_choice() == "Answer text A"
+    observation, reward, done, info = env.step(env.action_space.action_to_ix("CONTINUE"))
+    assert observation.get_facts() == sample.facts
+    assert observation.get_question() == sample.question
+    assert observation.get_choice() == "Answer text B"
+    assert not done
+    assert reward == 0.0
+    observation, reward, done, info = env.step(env.action_space.action_to_ix("CONTINUE"))
+    assert reward == 0.0
+    assert not done
+    observation, reward, done, info = env.step(env.action_space.action_to_ix("CONTINUE"))
+    assert reward == 0.0
+    assert done
