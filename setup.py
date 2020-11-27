7297bbbdef26317ed0103cb74668e99aa2f2cb7c
Rajkumar R <raj1514@gmail.com>
2020-11-27 18:15:06 +0100

    Added pypi workflow

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name="nlp_gym",
-      version="0.0.1",
+      version="0.1.0",
       description="NLPGym - A toolkit for evaluating RL agents on Natural Language Processing Tasks",
       author="Rajkumar Ramamurthy",
       author_email="raj1514@gmail.com",
       packages=find_packages(),
+      python_requires='>=3.7',
+      url="https://github.com/rajcscw/nlp-gym/",
       install_requires=["numpy", "torch", "nltk", "flair", "pandas", "matplotlib",
                         "seaborn", "sklearn", "tqdm", "edit_distance", "rich",
                         "gym", "pytorch-nlp", "datasets", "wget"],
