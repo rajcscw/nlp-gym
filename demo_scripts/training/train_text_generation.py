d5a8a8ffea262a59a5f8fb801b7c3b11c7b2904a
Rajkumar <raj1514@gmail.com>
2022-04-08 10:45:50 +0000

    Updates for new datasets

--- demo_scripts/training/train_text_generation.py
+++ demo_scripts/training/train_text_generation.py
@@ -1,10 +1,13 @@
-from nlp_gym.data_pools.custom_text_generation_pools import CommonGen, Sample, Xsum
+from nlp_gym.data_pools.custom_text_generation_pools import CommonGen, Sample, Xsum, CNNDailyMail
 from nlp_gym.envs.text_generation.env import TextGenEnv
-from nlp_gym.envs.text_generation.reward import MeteorRewardFunctionWithKL
+from nlp_gym.envs.text_generation.reward import MeteorRewardFunctionWithKL, MeteorRewardFunction
 from stable_baselines3.common.env_checker import check_env
 from transformers import AutoTokenizer
 from stable_baselines3.ppo.ppo import PPO
 from nlp_gym.envs.text_generation.policy import LMActorCriticPolicy
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from rich import print
 
 
 def run_episode(model: PPO, env: TextGenEnv, data_pool: CommonGen, sample: Sample = None):
@@ -18,35 +21,39 @@ def run_episode(model: PPO, env: TextGenEnv, data_pool: CommonGen, sample: Sampl
         action, state = model.predict(obs, state)
         obs, reward, done, info = env.step(action)
     print(reward)
-    print(info)
+    print(info["output"])
 
 
-# data pool
-data_pool = Xsum.prepare(split="train")
+if __name__ == "__main__":
 
-# reward function
-model_name = "distilgpt2"
-reward_fn = MeteorRewardFunctionWithKL()
+    # data pool
+    data_pool = CNNDailyMail.prepare(split="train")
+    samples = [(sample, weight) for sample, weight in data_pool]
 
-# text generation env
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-env = TextGenEnv(tokenizer, reward_fn, max_text_length=500, max_steps=100)
-for sample, weight in data_pool:
-    env.add_sample(sample, weight)
+    # reward function
+    model_name = "distilgpt2"
+    train_reward_fn = MeteorRewardFunctionWithKL()
+    eval_reward_fn = MeteorRewardFunction()
 
-# test sample
-test_sample = data_pool.sample()
+    # text generation env
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    train_env = TextGenEnv(tokenizer, eval_reward_fn,
+                           max_text_length=500, max_steps=100,
+                           samples=samples)
+    eval_env = TextGenEnv(tokenizer, eval_reward_fn,
+                          max_text_length=500, max_steps=100,
+                          samples=samples)
 
-# instantiate the PPO alg with the model
-model = PPO(policy=LMActorCriticPolicy, env=env, policy_kwargs={
-    "model_name": model_name,
-}, n_steps=128, batch_size=16, verbose=1, learning_rate=1e-7, n_epochs=5)
-reward_fn.set_policy(model.policy)
+    # instantiate the PPO alg with the model
+    model = PPO(policy=LMActorCriticPolicy, env=train_env, policy_kwargs={
+        "model_name": model_name,
+        "apply_model_parallel": True,
+    }, n_steps=200, batch_size=32, verbose=1, learning_rate=1e-6, n_epochs=5, ent_coef=1e-2)
+    train_reward_fn.set_policy(model.policy)
 
+    run_episode(model, eval_env, data_pool)
 
-run_episode(model,  env, data_pool, test_sample)
-
-# train
-for i in range(500):
-    model.learn(256)
-    run_episode(model,  env, data_pool)
+    # train
+    for i in range(500):
+        model.learn(200)
+        run_episode(model, eval_env, data_pool)
--- nlp_gym/data_pools/custom_text_generation_pools.py
+++ nlp_gym/data_pools/custom_text_generation_pools.py
@@ -22,15 +22,38 @@ class CommonGen(TextGenPool):
 
 class Xsum(TextGenPool):
     @classmethod
-    def prepare(cls, split: str):
+    def prepare(cls, split: str, prompt_suffix: str = "TL;DR:"):
         dataset = load_dataset("gem", "xsum")
         dataset_split = dataset[split]
         samples = []
         for ix, item in enumerate(dataset_split):
             sample = Sample(id=f"{split}_{ix}",
-                            prompt_or_input_text=item["document"],
+                            prompt_or_input_text=item["document"] +
+                            prompt_suffix,
                             references=[item["target"]]
                             )
             samples.append(sample)
         pool_instance = cls(samples)
         return pool_instance
+
+
+class CNNDailyMail(TextGenPool):
+    @classmethod
+    def prepare(cls, split: str, prompt_suffix: str = "TL;DR:"):
+        dataset = load_dataset("cnn_dailymail", "3.0.0")
+        dataset_split = dataset[split]
+        samples = []
+        for ix, item in enumerate(dataset_split):
+            sample = Sample(id=f"{split}_{ix}",
+                            prompt_or_input_text=item["article"] +
+                            prompt_suffix,
+                            references=[item["highlights"]]
+                            )
+            samples.append(sample)
+        pool_instance = cls(samples)
+        return pool_instance
+
+
+if __name__ == "__main__":
+    ds = CNNDailyMail.prepare("train")
+    print(len(ds))
